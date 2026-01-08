24a0382b2b99e3862046d8f153bb1dd80416d0bd
Pankaj Panday <ppanday064@gmail.com>
2026-01-08 10:49:39 +0530

    expense tracker day 1

---
+++ screens/AllExpenses.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function AllExpenses() {
+  return (
+    <View>
+      <Text>AllExpenses</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
